135ce8b98bad32bd55d8bfa31b39ffa771a9510c
Tomohiro Noguchi <t.ngc.0403@gmail.com>
2022-01-23 09:59:50 +0000

    chore: node-app by Fetch API

--- node-app/index.mjs
+++ node-app/index.mjs
@@ -1,5 +1,6 @@
-import lodash from 'lodash'
+import fetch from 'node-fetch'
 
-const msg = lodash.flatten([["Hello"], ["Node.js"]]).join(", ")
+const host = process.env.PYTHON_APP_HOST || "localhost"
+const port = process.env.PYTHON_APP_PORT || "5000"
 
-console.log(msg)
+fetch(`http://${host}:${port}`).then(res => res.text()).then(console.log) // Hello, World!
